d6f88582e11f7c264b1d2b3db8d123403d5db54e
Azamat Tajiyew <muradow.azamat98@gmail.com>
2023-03-01 19:49:00 +0500

    catrgory and brand  added

--- seeders/20220918143306-category.js
+++ seeders/20220918143306-category.js
@@ -4,7 +4,7 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     let categories=[
       {
-        name:'{"tm":"gok onumler","ru":"gok onumler"}',
+        name:'{"tm":"Sowgatlyk harytlar","ru":"Идеи для подарков"}',
         description: '{"tm":"gok onumler barada ","ru":"gok onumler barada"}',
         parentId: null,
         active: 1,
@@ -13,8 +13,8 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        name:'{"tm":"suwlar","ru":"suwlar"}',
-        description: '{"tm":"suwlar barada ","ru":"suwlar barada"}',
+        name:'{"tm":"Parfýumeriýa","ru":"Парфюмерия"}',
+        description: '{"tm":"","ru":""}',
         parentId: null,
         active: 1,
         order: 2,
@@ -22,7 +22,25 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        name:'{"tm":"gok onumler un dokunler","ru":"gok onumler un dokunler"}',
+        name:'{"tm":"Smartfonlar we gadjetler","ru":"Смартфоны и гаджеты"}',
+        description: '{"tm":"","ru":""}',
+        parentId: null,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"Aýal gyzlar üçin","ru":"Подарки для женщин"}',
+        description: '{"tm":"suwlar barada ","ru":"suwlar barada"}',
+        parentId: 1,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"Erkekler üçin","ru":"Подарки для мужчин"}',
         description: '{"tm":"gok onumler un dokunler","ru":"gok onumler un dokunler"}',
         parentId: 1,
         active: 1,
@@ -31,8 +49,89 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        name:'{"tm":"ter miweleler","ru":"ter miweleler"}',
+        name:'{"tm":"Sowgat sertifikatlar","ru":"Подарочный сертификат"}',
         description: '{"tm":"ter miweleler barada","ru":"ter miweleler barada"}',
+        parentId: 1,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"Erkekler üçin","ru":"Для него"}',
+        description: '{"tm":"","ru":""}',
+        parentId: 2,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"Aýal gyzlar üçin","ru":"Для нее"}',
+        description: '{"tm":"","ru":""}',
+        parentId: 2,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"Akylly sagatlar","ru":"Умные часы"}',
+        description: '{"tm":"","ru":""}',
+        parentId: 3,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"Aksesuarlar","ru":"Аксессуары для телефонов и планшетов"}',
+        description: '{"tm":"","ru":""}',
+        parentId: 3,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"Mobil telefonlar we planşetler","ru":"Мобильные телефоны и планшеты"}',
+        description: '{"tm":"","ru":""}',
+        parentId: 3,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"","ru":""}',
+        description: '{"tm":"","ru":""}',
+        parentId: 1,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"","ru":""}',
+        description: '{"tm":"","ru":""}',
+        parentId: 1,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"","ru":""}',
+        description: '{"tm":"","ru":""}',
+        parentId: 1,
+        active: 1,
+        order: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'{"tm":"","ru":""}',
+        description: '{"tm":"","ru":""}',
         parentId: 1,
         active: 1,
         order: 2,
